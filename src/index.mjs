acf2e7710bc67922271dfd85e9adfa8a58729de5
keshavsaini07 <sainikeshav770@gmail.com>
2024-06-06 00:11:50 +0530

    added signed cookies for product routes

--- src/index.mjs
+++ src/index.mjs
@@ -6,13 +6,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser("anything"));
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.cookie("cookkie", "hello world", { maxAge: 10000, signed: true }  )
+    res.cookie("cookkie", "hello world", { maxAge: 60000, signed: true }  )
   res.status(201).send({ msg: "Hello World" });
 });
 
--- src/routes/product-routes.mjs
+++ src/routes/product-routes.mjs
@@ -4,8 +4,9 @@ const router = Router();
 
 router.get("/api/products", (req, res) => {
   console.log(req.headers.cookie);
-  console.log(req.cookies)
-  if (req.cookies.cookkie && req.cookies.cookkie === "hello world")
+  console.log(req.cookies);
+  console.log(req.signedCookies.cookkie)
+  if (req.signedCookies.cookkie && req.signedCookies.cookkie === "hello world")
     return res.send([{ id: 101, name: "Dosa", price: 60 }]);
 
   return res.status(403).send({msg: "Sorry! Please retry with a correct cookie."})
